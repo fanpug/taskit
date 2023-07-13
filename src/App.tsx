c749f624605e4b6cc40592e0668e73e38f498b70
Humberto Navarro <a320669@uach.mx>
2023-07-13 16:19:40 -0600

    Add todo state to app and props

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
 import InputField from './components/InputField'
 
 const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+
   return (
     <div className='App'>
       <span className='heading'>TaskIt</span>
-      <InputField />
+      <InputField todo={todo} setTodo={setTodo} />
     </div>
   )
 }
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,9 +1,20 @@
 import "./styles.css"
 
-const InputField = () => {
+interface Props {
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const InputField = ({todo, setTodo}: Props) => {
   return (
     <form className="input">
-        <input type="input" placeholder='Enter a task' className='input__box' />
+        <input 
+            value={todo}
+            onChange={(e) => setTodo(e.target.value)}
+            type="input" 
+            placeholder='Enter a task' 
+            className='input__box' 
+        />
         <button type="submit" className='input_submit'>Go</button>
     </form>
   )
